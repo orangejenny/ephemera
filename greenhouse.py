c138380df779c5533035d1aef7a7d1716eddb093
Jennifer Schweers <orange.jenny@gmail.com>
2017-08-22 22:25:31 -0400

    Re-organize data structures

--- greenhouse.py
+++ greenhouse.py
@@ -11,18 +11,30 @@ from requests.auth import HTTPBasicAuth
 parser = argparse.ArgumentParser(description='Analyze applicant data')
 parser.add_argument('--token', help='API token')
 parser.add_argument('--refresh', action='store_true', help='Overwrite data files')
+parser.add_argument('--verbose', action='store_true', help='Print verbose debugging')
 arguments = parser.parse_args(sys.argv[1:])
+printer = pprint.PrettyPrinter(indent=4)    # pp.pprint(stuff)
 
 BASE_URL = "https://harvest.greenhouse.io/v1/"
 TOKEN = arguments.token
-REFRESH_DATA = arguments.refresh
 DATA_DIR = "data"
-printer = pprint.PrettyPrinter(indent=4)    # pp.pprint(stuff)
+
+DEV_INTERVIEW_STAGES = {
+    'Dev Round 1 - Phone Screen': 0,
+    'Dev Round 1 - Technical Interview': 0,
+    'Dev Round 2 - Non-Technical Interview': 1,
+    'Dev Round 2 - Technical Interview 1': 1,
+    'Dev Round 2 - Technical Interview 2': 1,
+    'Dev Second Round Technical Interview': 1,
+    'Dev Peer Panel Review': 2,
+}
+DEV_INTERVIEW_TYPES = DEV_INTERVIEW_STAGES.keys()
 
 def list_things(type):
     filename = "{}/{}.json".format(DATA_DIR, type)
-    if os.path.isfile(filename) and not REFRESH_DATA:
-        print "reading from {}".format(filename)
+    if os.path.isfile(filename) and not arguments.refresh:
+        if arguments.verbose:
+            print "reading from {}".format(filename)
         with open(filename) as data:
             all_things = json.load(data)
     else:
@@ -32,7 +44,8 @@ def list_things(type):
         while page == 1 or len(things):
             response = requests.get("{}/{}/?per_page=500&page={}".format(BASE_URL, type, page), auth=HTTPBasicAuth(TOKEN, ''))
             things = json.loads(response.text)
-            print "fetching page {} of {}".format(page, type)
+            if arguments.verbose:
+                print "fetching page {} of {}".format(page, type)
             if response.status_code != 200:
                 raise Exception("{}: {}".format(response.status_code, response.text))
             all_things = all_things + things
@@ -40,7 +53,8 @@ def list_things(type):
         with open(filename, 'w') as outfile:
             json.dump(all_things, outfile)
 
-    print "found {} {}".format(len(all_things), type)
+    if arguments.verbose:
+        print "found {} {}".format(len(all_things), type)
     return all_things
 
 def binary_result(scorecard):
@@ -50,166 +64,135 @@ def binary_result(scorecard):
         return '0'
     return ''
 
-def calibrate_interviewers(scorecards, scorecard_filter=None):
-    interviewers = {}
-    for s in dev_scorecards:
-        if not scorecard_filter or scorecard_filter(s):
-            name = s['submitted_by']['name']
-            if name not in interviewers:
-                interviewers[name] = ''
-            interviewers[name] = interviewers[name] + binary_result(s)
-    return interviewers
+def percent_of(part, whole):
+    return int(part * 100 / whole + 0.5)
 
+
+
+
+# Fetch data, filter down to dev scorecards only
 departments = list_things('departments')
 tech_department_id = next(d['id'] for d in departments if d['name'].lower() == 'tech')
-
 jobs = list_things('jobs')
 tech_job_ids = set([j['id'] for j in jobs if tech_department_id in [d['id'] for d in j['departments']]])
-
-applications = list_things('applications')  # alternatively, could list the applications for each tech job id
+applications = list_things('applications')
 tech_application_ids = set([a['id'] for a in applications if set([j['id'] for j in a['jobs']]) & tech_job_ids])
-
-#candidates = list_things('candidates')
-#tech_candidate_ids = [c['id'] for c in candidates if set([a['id'] for a in c['applications']]) & tech_application_ids]
-
 scorecards = list_things('scorecards')
 tech_scorecards = [s for s in scorecards if s['application_id'] in tech_application_ids]
+dev_scorecards = [s for s in tech_scorecards if s['interview'] in DEV_INTERVIEW_TYPES]
 
-tech_interview_types = set([s['interview'] for s in tech_scorecards])
-dev_scorecards = [s for s in tech_scorecards if re.search(r'^Dev ', s['interview']) and not re.search(r'Application Review', s['interview'])]
-dev_interview_types = set([s['interview'] for s in dev_scorecards])
-
-dev_scorecards_by_interview_type = {}
-for t in dev_interview_types:
-    dev_scorecards_by_interview_type[t] = [s for s in dev_scorecards if s['interview'] == t]
-    print "{}: {} scorecards".format(t, len(dev_scorecards_by_interview_type[t]))
-
-phone_screen_scorecards = []
-second_round_scorecards = []
-final_round_scorecards = []
-for type, scorecards in dev_scorecards_by_interview_type.iteritems():
-    if re.search(r'round 1', type.lower()):
-        phone_screen_scorecards = phone_screen_scorecards + scorecards
-    elif re.search(r'round 2', type.lower()) or re.search(r'second round', type.lower()):
-        second_round_scorecards = second_round_scorecards + scorecards
-    elif type == 'Dev Peer Panel Review':
-        final_round_scorecards = final_round_scorecards + scorecards
-
-# Pass rates on each round
-phone_screen_candidates = set([s['candidate_id'] for s in phone_screen_scorecards])
-second_round_candidates = set([s['candidate_id'] for s in second_round_scorecards])
-final_round_candidates = set([s['candidate_id'] for s in final_round_scorecards])
-final_round_applications = set([s['application_id'] for s in final_round_scorecards])
 applications_by_id = {a['id']: a for a in applications}
-final_round_applications_not_rejected = [a for a in final_round_applications if applications_by_id[a]['status'] != 'rejected']
+DEV_APPLICATIONS = {}
+for s in dev_scorecards:
+    if s['application_id'] not in DEV_APPLICATIONS:
+        DEV_APPLICATIONS[s['application_id']] = {
+            'stages': [
+                [],     # phone screen scorecard
+                [],     # second round scorecards
+                [],     # final round scorecards
+            ],
+            'status': applications_by_id[s['application_id']]['status'],    # note that 'rejected' might mean candidate declined offer,
+                                                                            # which makes all stats related to this field suspect
+        }
+    DEV_APPLICATIONS[s['application_id']]['stages'][DEV_INTERVIEW_STAGES[s['interview']]].append(s)
+DEV_APPLICATIONS_NOT_REJECTED = {id: a for id, a in DEV_APPLICATIONS.iteritems() if len(a['stages'][2]) and a['status'] != 'rejected'}
 
-print ""
-print "FUNNEL"
-print "{} phone screens".format(len(phone_screen_candidates))
-print "{} second rounds ({}% of phone screens)".format(len(second_round_candidates), math.floor(len(second_round_candidates) * 100 / len(phone_screen_candidates) + 0.5))
-print "{} final rounds ({}% of second rounds)".format(len(final_round_candidates), math.floor(len(final_round_candidates) * 100 / len(second_round_candidates) + 0.5))
-print "{}% of final rounds passed (or are still active)".format(len(final_round_applications_not_rejected) * 100 / len(final_round_candidates))
-print ""
 
-# Phone screen question frequency
-print ""
-print "PHONE SCREENS"
-phone_screen_questions = {}     # id => { questions => set(), answers => { 'answer' => count } }
-for s in phone_screen_scorecards:
-    for q in s['questions']:
-        if q['id']:     # the notes questions don't have ids
-            if q['answer'] in ['Average', 'Strong', 'Weak']:
-                name = q['question'].lower()
-                name = re.sub(r'^q[0-9]*\s*:?', '', name)
-                if name not in phone_screen_questions:
-                    phone_screen_questions[name] = {}
-                if q['answer'] not in phone_screen_questions[name]:
-                    phone_screen_questions[name][q['answer']] = 0
-                phone_screen_questions[name][q['answer']] = phone_screen_questions[name][q['answer']] + 1
-printer.pprint(phone_screen_questions)
+# Funnel of stages
+totals = [0, 0, 0]
+for i in range(3):
+    totals[i] = len([a for a in DEV_APPLICATIONS.values() if len(a['stages'][i])])
+
+print "\nFUNNEL"
+print "{} phone screens".format(totals[0])
+print "{} second rounds ({}% of phone screens)".format(totals[1], percent_of(totals[1], totals[0]))
+print "{} final rounds ({}% of second rounds)".format(totals[2], percent_of(totals[2], totals[1]))
+print "{} final rounds not rejected ({}% of final rounds)".format(len(DEV_APPLICATIONS_NOT_REJECTED), percent_of(len(DEV_APPLICATIONS_NOT_REJECTED), totals[2]))
+
 
 # Frequency with which 2nd round tech interviews disagree
-technical_second_round_results = {}
-for s in second_round_scorecards:
-    if not re.search(r'non.technical', s['interview'].lower()):
-        cid = s['candidate_id']
-        result = ''
-        if cid in technical_second_round_results:
-            result = technical_second_round_results[cid]
-        technical_second_round_results[cid] = result + binary_result(s)
-passed_both = set([cid for cid, result in technical_second_round_results.iteritems() if result == '11'])
-failed_both = set([cid for cid, result in technical_second_round_results.iteritems() if result == '00'])
-disagreements = set([cid for cid, result in technical_second_round_results.iteritems() if result in ['01', '10']])
-total = len([cid for cid, result in technical_second_round_results.iteritems() if len(result) == 2])
-final_round_candidates_not_rejected = set([applications_by_id[a]['candidate_id'] for a in final_round_applications_not_rejected])
+technical_results = {}
+nontechnical_results = {}
+for id, a in DEV_APPLICATIONS.iteritems():
+    for s in a['stages'][1]:
+        if re.search(r'non.technical', s['interview'].lower()):
+            nontechnical_results[id] = binary_result(s)
+        else:
+            if id not in technical_results:
+                technical_results[id] = ''
+            technical_results[id] = technical_results[id] + binary_result(s)
 
-print ""
-print "SECOND ROUND TECHNICAL ({} total interviews)".format(total)
-print "{} passed both ({}%)".format(len(passed_both), math.floor(len(passed_both) * 100 / total + 0.5))
-print "{} failed both ({}%)".format(len(failed_both), math.floor(len(failed_both) * 100 / total + 0.5))
-print "{} disagreed ({}%)".format(len(disagreements), math.floor(len(disagreements) * 100 / total + 0.5))
-print "Of the disagreements, {}% went on to final round".format(math.floor(len(disagreements & final_round_candidates) * 100 / len(disagreements) + 0.5))
-print "Of the disagreements, {}% were ultimately rejected".format(math.floor((len(disagreements) - len(disagreements & final_round_candidates_not_rejected)) * 100 / len(disagreements) + 0.5))
-print ""
 
-print "FINAL ROUNDS"
-final_round_results = {}
-outcomes = {
-    'definitely_not': 'a',
-    'no': 'b',
-    'yes': 'c',
-    'strong_yes': 'd',
-}
-for s in final_round_scorecards:
-    if s['overall_recommendation'] in outcomes:
-        if s['application_id'] not in final_round_results:
-            final_round_results[s['application_id']] = []
-        final_round_results[s['application_id']].append(outcomes[s['overall_recommendation']])
-final_round_results_sorted = [{
-    'status': applications_by_id[application_id]['status'],
-    'results': "".join(sorted(results)),
-} for application_id, results in final_round_results.iteritems()]
-final_round_results_sorted.sort(key=lambda x: x['results'])
-for final_round in final_round_results_sorted:
-    print "{} => {}".format(final_round['results'].replace('a', '.').replace('b', '0').replace('c', '1').replace('d', '*'), final_round['status'])
-print ""
+passed_both = len([id for id, result in technical_results.iteritems() if result == '11'])
+failed_both = len([id for id, result in technical_results.iteritems() if result == '00'])
+disagreements = [id for id, result in technical_results.iteritems() if result in ['01', '10']]
+total = len([id for id, result in technical_results.iteritems() if len(result) == 2])
+
+print "\nSECOND ROUND TECHNICAL ({} total interviews)".format(total)
+print "{} passed both ({}%)".format(passed_both, percent_of(passed_both, total))
+print "{} failed both ({}%)".format(failed_both, percent_of(failed_both, total))
+print "{} disagreed ({}%)".format(len(disagreements), percent_of(len(disagreements), total))
+print "Of the disagreements, {}% went on to final round".format(percent_of(len(set(disagreements) & set([id for id, a in DEV_APPLICATIONS.iteritems() if a['stages'][2]])), len(disagreements)))
+print "Of the disagreements, {}% ultimately succeeded".format(percent_of(len(set(disagreements) & set(DEV_APPLICATIONS_NOT_REJECTED)), len(disagreements)))
+
+print "\nSECOND ROUND NON-TECHNICAL"
+print "{}% passed (of {} total non-technical interviews)".format(percent_of(len([r for r in nontechnical_results.values() if r == '1']), len(nontechnical_results)), len(nontechnical_results))
+
+
+print "\nFINAL ROUNDS"
+
+final_round_results = []
+outcomes = ['definitely_not', 'no', 'yes', 'strong_yes']
+for a in DEV_APPLICATIONS.values():
+    if a['stages'][2]:
+        final_round_results.append({
+            'application': a,
+            'results': "".join(sorted(['1' if re.search(r'yes', s['overall_recommendation']) else '0' for s in a['stages'][2] if s['overall_recommendation'] in outcomes])),
+        })
+final_round_results.sort(key=lambda x: x['results'])
+for final_round in final_round_results:
+    print "{} \t{}=> {}".format(final_round['results'], ("\t" if len(final_round['results']) < 7 else ""), final_round['application']['status'])
+
 
 
 # Pass rates for specific users
+interviewers = {}
+for a in DEV_APPLICATIONS.values():
+    for i, stage in enumerate(a['stages']):
+        for scorecard in stage:
+            name = scorecard['submitted_by']['name']
+            if name not in interviewers:
+                interviewers[name] = [[], [], []]
+            interviewers[name][i].append(scorecard)
 
-print "INTERVIEWERS"
-def _scorecard_is_final(s):
-    return s['interview'] == 'Dev Peer Panel Review'
-def _scorecard_is_not_final(s):
-    return s['interview'] != 'Dev Peer Panel Review'
-all_interviewers = calibrate_interviewers(dev_scorecards)
-final_round_interviewers = calibrate_interviewers(dev_scorecards, scorecard_filter=_scorecard_is_final)
-non_final_round_interviewers = calibrate_interviewers(dev_scorecards, scorecard_filter=_scorecard_is_not_final)
-for name, results in all_interviewers.iteritems():
-    non_final_round_stats = "no non-final rounds"
-    if name in non_final_round_interviewers:
-        non_final_round_stats = "{}% over {} non-final rounds".format(
-            len(non_final_round_interviewers[name].replace("0", "")) * 100 / len(non_final_round_interviewers[name]), len(non_final_round_interviewers[name]),
-        )
-    final_round_stats = "no final rounds"
-    if name in final_round_interviewers:
-        final_round_stats = "{}% over {} final rounds".format(
-            len(final_round_interviewers[name].replace("0", "")) * 100 / len(final_round_interviewers[name]), len(final_round_interviewers[name])
-        )
-    print "{} has a {}% pass rate over {} interviews ({}, {})".format(name, len(results.replace("0", "")) * 100 / len(results), len(results), non_final_round_stats, final_round_stats)
-print ""
+print "\nINTERVIEWERS"
+stage_names = ["phone screens", "second rounds", "final rounds"]
+interviewer_stats = []
+for name, results in interviewers.iteritems():
+    stage_stats = []
+    for i, stage in enumerate(stage_names):
+        stage_stats.append("no " + stage)
+        if results[i]:
+            passes = len([s for s in results[i] if re.search(r'yes', s['overall_recommendation'])])
+            total = len(results[i])
+            stage_stats[i] = "{}% of {} {}".format(percent_of(passes, total), total, stage)
 
+    passes = len([scorecard for stages in results for scorecard in stages if re.search(r'yes', scorecard['overall_recommendation'])])
+    total = len([scorecard for stages in results for scorecard in stages])
+    interviewer_stats.append({
+        'stat': "{} has a {}% pass rate over {} interviews ({}, {}, {})".format(
+            name,
+            percent_of(passes, total),
+            total,
+            stage_stats[0],
+            stage_stats[1],
+            stage_stats[2]
+        ),
+        'overall': percent_of(passes, total),
+    })
+interviewer_stats.sort(key=lambda x: x['overall'])
+for i in interviewer_stats:
+    print i['stat']
 
 # TODO: phone screen: frequency with which questions are asked
-# TODO: phone screen: how well do question responses correlate with eventual success?
-
-# TODO: How often does an earlier interviewer change their mind by the final round? (Should they bother coming?)
-
-# TODO: Filter interview types better, limit to exact the expected types
-# TODO: deal with applications, not candidates
-# TODO: move each section into a function to more easily comment out
-'''
-- has an id
-- has a series of stages, each with 1 or more scorecards
-- has an overall status
-'''
+# TODO: phone screen: how well do question responses correlate with later success?
